d513e6aa8b5043efeb3d84816a1b8dc59e89bbb7
zabaidulbd <zislam5622@gmail.com>
2023-06-27 00:31:07 +0600

    add no animation

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "../Banner/Banner";
+import Experience from "../Experience/Experience";
 
 
 const Home = () => {
     return (
         <>
             <Banner></Banner>
+            <Experience></Experience>
         </>
     );
 };
