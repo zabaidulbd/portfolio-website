96d1546776ed2059ab9621f40c84c29b66f7c6d6
zabaidulbd <zislam5622@gmail.com>
2023-06-26 13:16:51 +0600

    add about

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import {
 } from "react-router-dom";
 import Main from './layout/Main';
 import Home from './pages/Home/Home/Home';
+import About from './pages/About/About';
 
 
 const router = createBrowserRouter([
@@ -17,6 +18,10 @@ const router = createBrowserRouter([
       {
         path: '/',
         element: <Home></Home>
+      },
+      {
+        path: 'about',
+        element: <About></About>
       }
     ]
   },
---
+++ src/pages/About/About.jsx
@@ -0,0 +1,19 @@
+import authorImg from '../../assets/Images/Author/successful-businessman.jpg'
+
+const About = () => {
+    return (
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row">
+                <img src={authorImg} className="max-w-lg rounded-lg shadow-2xl" />
+                <div>
+                    <h1 className="text-5xl font-bold">Zabaidul Islam</h1>
+                    <h2 className='text-3xl font-semibold'>MERN Developer</h2>
+                    <p className="py-6">Hello! I am Md. Zabaidul Islam, a Junior MERN developer passionate about creating web applications. With a focus on MERN (MongoDB, Express.js, React, and Node.js) stack, I enjoy building interactive and user-friendly websites. My goal is to contribute my skills and creativity to a supportive and growth-oriented team, where I can continue to learn and make a meaningful impact. I am eager to take on new challenges, broaden my horizons, and create exceptional web experiences.Feel free to explore my portfolio to see some of my projects and get in touch if you have any exciting opportunities or ideas to discuss. Let's build something great together</p>
+                    <button className="btn btn-primary">Contact</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default About;
