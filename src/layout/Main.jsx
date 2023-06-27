3521fdac67fa59698a3b4b8fd4f74441b66c9f63
zabaidulbd <zislam5622@gmail.com>
2023-06-27 12:44:48 +0600

    add footer

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,12 +1,14 @@
 
 import { Outlet } from 'react-router-dom';
 import Navbar from '../shared/Navbar/Navbar';
+import Footer from '../shared/Footer/Footer';
 
 const Main = () => {
     return (
         <>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </>
     );
 };
