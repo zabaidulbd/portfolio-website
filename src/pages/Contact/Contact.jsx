1ba04b724eaced9b9835304a53fb9b3390ff0419
zabaidulbd <zislam5622@gmail.com>
2023-06-27 16:38:33 +0600

    add contact form

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -2,24 +2,24 @@
 
 const Contact = () => {
     return (
-        <>
-            <h1 className="text-5xl text-center font-bold">Contact</h1>
-            <div className="form-control w-full max-w-xs">
+        <div className="mt-20">
+            <h1 className="text-5xl text-center font-bold mb-14">Contact</h1>
+            <div className="form-control w-full max-w-lg mx-auto">
                 <label className="label">
-                    <span className="label-text">Please type your name</span>
+                    <span className="text-xl font-semibold">Please type your name</span>
                 </label>
-                <input type="text" placeholder="Your name" className="input input-bordered w-full max-w-xs" />
+                <input type="text" placeholder="Your name" className="input input-bordered border-8 w-full max-w-lg my-4" />
                 <label className="label">
-                    <span className="label-text">Please type your email</span>
+                    <span className="text-xl font-semibold">Please type your email</span>
                 </label>
-                <input type="email" placeholder="Your email" className="input input-bordered w-full max-w-xs" />
+                <input type="email" placeholder="Your email" className="input input-bordered border-8 w-full max-w-lg my-4" />
                 <label className="label">
-                    <span className="label-text">Your message</span>
+                    <span className="text-xl font-semibold">Your message</span>
                 </label>
-                <textarea className="textarea textarea-primary" placeholder="Your message"></textarea>
-                <button className="btn btn-active btn-neutral">send</button>
+                <textarea className="textarea textarea-primary my-4" placeholder="Your message"></textarea>
+                <button className="btn btn-active btn-neutral mb-10">send</button>
             </div>
-        </>
+        </div>
     );
 };
 
